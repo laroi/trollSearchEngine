c7d372e805668a0ed1ad3c0f131744b0cafed211
akbar <akbarali1klr@gmail.com>
2019-02-11 19:12:05 +0530

    Migration scripts started

---
+++ migration/export.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient;
+const path = require('path');
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB = process.env.DB || 'trolls'
+const RES_LOC = '';
+const FILE_LOC = path.join(__dirname, 'upload');
+const FILE_NAME = 'db.json'
+const mongodbUrl = `mongodb://${DB_HOST}:${DB_PORT}/${DB}`;
+const file = fs.createWriteStream(path.join(FILE_LOC, FILE_NAME));
+const Transform = require('stream').Transform;
+const tar = require('tar');
+const ncp = require('ncp').ncp;
+ncp.limit = 16;
+const formatter = new Transform({
+    writableObjectMode: true,
+    transform(row, encoding, callback) {
+        return callback(null, JSON.stringify(row).toString());
+    }
+});
+const createFolder = (loc) => {
+    if (!fs.existsSync(loc)){
+            fs.mkdirSync(loc);
+    } else {
+        fs.rmdirSync(loc);
+        fs.mkdirSync(loc);
+    }
+}
+const tarFolder = (loc) => {
+	return tar.c(
+	  {
+		gzip: true,
+		file: 'my-tarball.tgz'
+	  },
+	  [loc]
+	)
+}
+createFolder()
+MongoClient.connect(mongodbUrl)
+    .then((client)=> {
+        var db = client.db(DB);
+        let postCollection = db.collection('posts');
+        let stream = postCollection.find().stream();
+        stream.on('end', function() {
+            console.log('finished writing db file');
+            db.close();
+            ncp(RES_LOC, FILE_LOC, function (err) {
+                 if (err) {
+                    return console.error(err);
+                 }
+                 console.log('Copied Resource Dir');
+				 tarFolder(FILE_LOC)
+					.then(()=> {
+						console.log('Completed tarring');
+					})
+					.catch((err) => {
+						console.error('Error in tarring');
+					})
+            });
+        });
+        stream.pipe(formatter).pipe(file);
+        stream.on('error', (err) => {
+            console.error(err);
+        });
+
+    })
+    .catch((err) => {
+        console.error('Couldn\'t connect to database ', err);
+    })
+

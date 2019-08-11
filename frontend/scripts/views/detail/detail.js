4be44fb21b8ed537a138e2e42e1c275c423a29df
Akbar <akbarali1klr@gmail.com>
2019-08-11 17:19:28 +0530

    destructuring bug fixed

--- frontend/scripts/views/detail/detail.js
+++ frontend/scripts/views/detail/detail.js
@@ -192,14 +192,12 @@ var animOutClass = "bounceInRight";
         }*/
         let sharePost = async (e) => {
                 let id = $(e.currentTarget).attr('data-post');
-                let file;
-                let postObj;
                 const svgElem = document.querySelector('.share-icon-path');   
                  if (navigator.canShare) {
                     svgElem.classList.add('share-anim');
                     postCollection.getPostById(id, async(err, post)=> {
                         try {
-                            {file, postObj} = await post.downloadOrShare('share');
+                            var {file, postObj} = await post.downloadOrShare('share');
                             svgElem.classList.remove('share-anim');
                             $(e.currentTarget).next().empty().html(postObj.shares.length);
                             
